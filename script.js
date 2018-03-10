bb01e34ca9727ba94cdcb0ea0f0b5f97caedae97
TheSupport101 <jpduarte120@gmail.com>
2018-03-10 21:28:06 +0000

    Finished clock.

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ function displayTime() {
     var h = today.getHours();
     var m = today.getMinutes();
     // ensuring format is hh:mm
-    if (h < 10) h = "0" + h;
-    if (m < 10) m = "0" + m;
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
     document.getElementById("clock").innerHTML = "<p>" + h + ":" + m + "</p>";
     //clock.innerHTML = h + ":" + m;
     t = setTimeout(function() {
